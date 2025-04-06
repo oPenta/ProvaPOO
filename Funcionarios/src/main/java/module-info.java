8ad95749033e2f69eb004af24f9e17ff3764bee3
Penta <142025868+oPenta@users.noreply.github.com>
2025-04-06 13:24:08 -0300

    Delete Funcionarios directory

--- Funcionarios/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.funcionarios {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.funcionarios to javafx.fxml;
-    exports com.example.funcionarios;
-}
